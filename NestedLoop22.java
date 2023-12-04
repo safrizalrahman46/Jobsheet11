9586dd2b4d0112b9494817ea05cd21578e9428b9
KazeKagi <safrizalrahman46@gmail.com>
2023-12-04 18:43:02 +0700

    Updatee Modify

---
+++ NestedLoop22.java
@@ -0,0 +1,28 @@
+/**
+ * NestedLoop22
+ */
+import java.util.Scanner;
+// import.java.util.array;
+public class NestedLoop22 {
+
+    public static void main(String[] args) {
+        Scanner ItingsMeledakbboomah22 = new Scanner(System.in); 
+
+            System.out.printf("Input Nilai N =");
+        int N = ItingsMeledakbboomah22.nextInt();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+            System.out.print("*");
+        }
+            System.out.println();
+            
+        }
+    }
+    //     System.out.printf("Input Nilai N =");
+    //     int N = ItingsMeledakbboomah22.nextInt();
+
+    //     for(int i= 0; i<=N; i++){
+    //         System.out.printf("<");
+    //     }
+    // }
+}
